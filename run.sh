0d9102940e4028d5339f67dfe1d11756de434e36
nekorro <nekorro@gmail.com>
2022-12-04 18:56:26 +0300

    fix image not refreshing in koyeb

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+echo "Cloning repo https://github.com/guchiko/momiji.git"
+git clone https://github.com/guchiko/momiji.git
+echo "Installing python reqs"
+pip install -r /momiji/requirements.txt
 echo "Preparing hole for HC..."
 nc -l -k -p 12345 -e echo &
-echo "Done!"
 echo "Script start"
-python main.py
+python /momiji/main.py
